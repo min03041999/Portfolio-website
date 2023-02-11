b80e88c99f9742a9b9642b3095b916f6087cbd86
Min Kookie <min03041999@gmail.com>
2023-02-11 15:12:43 +0700

    feat(footer): finished UI footer

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,14 +1,14 @@
-import { Alert, Col, Row } from "react-bootstrap";
 import { useState, useEffect } from "react";
+import { Col, Row, Alert } from "react-bootstrap";
 
-export const Newsletter = ({ onValidated, status, message }) => {
+export const Newsletter = ({ status, message, onValidated }) => {
   const [email, setEmail] = useState("");
 
   useEffect(() => {
     if (status === "success") clearFields();
   }, [status]);
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     email &&
       email.indexOf("@") > -1 &&
@@ -20,24 +20,27 @@ export const Newsletter = ({ onValidated, status, message }) => {
   const clearFields = () => {
     setEmail("");
   };
+
   return (
     <Col lg={12}>
-      <div className="news-letter-bx">
+      <div className="newsletter-bx wow slideInUp">
         <Row>
-          <Col lg={12} md={6} xl={5}>
-            <h3>Subscribe to our Newsletter</h3>
+          <Col lg={12} md={12} xl={5}>
+            <h3>
+              Subscribe to our Newsletter<br></br> & Never miss latest updates
+            </h3>
             {status === "sending" && <Alert>Sending...</Alert>}
             {status === "error" && <Alert variant="danger">{message}</Alert>}
             {status === "success" && <Alert variant="success">{message}</Alert>}
           </Col>
-          <Col md={6} xl={7}>
+          <Col lg={12} md={12} xl={7}>
             <form onSubmit={handleSubmit}>
-              <div className="new-mail-bx">
+              <div className="new-email-bx">
                 <input
                   value={email}
                   type="email"
                   onChange={(e) => setEmail(e.target.value)}
-                  placeholder="Email"
+                  placeholder="Email Address"
                 />
                 <button type="submit">Submit</button>
               </div>
